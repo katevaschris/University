11de3aad1e644955c1463373032d51be4b49c0cc
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-10-24 21:55:08 +0300

    Add files via upload

---
+++ 3rd/C#/Calculator/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.calculate = new System.Windows.Forms.Button();
+            this.result_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // calculate
+            // 
+            this.calculate.BackColor = System.Drawing.Color.Transparent;
+            this.calculate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.calculate.Location = new System.Drawing.Point(35, 98);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(89, 32);
+            this.calculate.TabIndex = 0;
+            this.calculate.Text = "=";
+            this.calculate.UseVisualStyleBackColor = false;
+            this.calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // result_label
+            // 
+            this.result_label.AutoSize = true;
+            this.result_label.BackColor = System.Drawing.SystemColors.Window;
+            this.result_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_label.ForeColor = System.Drawing.Color.Black;
+            this.result_label.Location = new System.Drawing.Point(214, 98);
+            this.result_label.Name = "result_label";
+            this.result_label.Size = new System.Drawing.Size(19, 25);
+            this.result_label.TabIndex = 1;
+            this.result_label.Text = " ";
+            this.result_label.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(24, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(201, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/",
+            "^",
+            "Round",
+            "Abs",
+            "√"});
+            this.comboBox1.Location = new System.Drawing.Point(135, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(60, 33);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(340, 210);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.result_label);
+            this.Controls.Add(this.calculate);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label result_label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ 3rd/C#/Calculator/Form1.cs
@@ -0,0 +1,111 @@
+﻿/*
+Author: Katevas Chris
+
+github: https://github.com/katevaschris
+	
+Planet: Earth 
+Date: 	
+	
+Version 1.0
+Function:
+
+Read more on: https://github.com/katevaschris/
+
+Copyright (C)
+Protected by GNU GPLv3
+
+~ 
+	
+	Greetings Mate!
+	¯\_(ツ)_/¯ 
+	
+	I don't mind what you use this code for! it wasn't made for professional use.
+	Instead, it was made for educational purposes and for fun.
+	Please hack this, change it and use it in any way you see fit.
+	Please, be acknowledged that I take no responsibility for anything bad that 
+	happens, to you as a result of your actions. However this code is protected by GNU GPLv3,
+	this means you must attribute me if you use it. Have fun and...
+	Cheers :D
+	
+~
+
+ */
+using System;
+using System.Windows.Forms;
+
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            double x = 0;
+            double y = 0;
+            if(textBox1.Text != "")
+            {
+                x = Convert.ToDouble(textBox1.Text);
+            }
+            if(textBox2.Text != "")
+            {
+                y = Convert.ToDouble(textBox2.Text);
+            }
+            
+            string symbol = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            if(symbol == "+")
+            {
+                result_label.Text = (Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text)).ToString();
+            }
+            else if(symbol == "-")
+            {
+                result_label.Text = (Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text)).ToString();
+            }
+            else if (symbol == "*")
+            {
+                result_label.Text = (Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text)).ToString();
+            }
+            else if (symbol == "/")
+            {
+                //You can actually divide with zero!
+                result_label.Text = (Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text)).ToString();
+            }
+            else if(symbol == "^")
+            {
+                result_label.Text = (Math.Pow(x, y)).ToString();
+            }
+            else if (symbol == "Round")
+            {
+                result_label.Text = (Math.Round(y)).ToString();
+            }
+            else if (symbol == "Abs")
+            {
+                result_label.Text = (Math.Abs(y)).ToString();
+            }
+            else// if (symbol == "√")
+            {
+                result_label.Text = (Math.Sqrt(y)).ToString();
+            }
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

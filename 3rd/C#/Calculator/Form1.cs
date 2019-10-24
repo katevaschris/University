12b520ba8e36222830c790e55518a2313e1dec05
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-10-24 21:57:18 +0300

    Update Form1.cs

--- 3rd/C#/Calculator/Form1.cs
+++ 3rd/C#/Calculator/Form1.cs
@@ -1,35 +1,3 @@
-﻿/*
-Author: Katevas Chris
-
-github: https://github.com/katevaschris
-	
-Planet: Earth 
-Date: 	
-	
-Version 1.0
-Function:
-
-Read more on: https://github.com/katevaschris/
-
-Copyright (C)
-Protected by GNU GPLv3
-
-~ 
-	
-	Greetings Mate!
-	¯\_(ツ)_/¯ 
-	
-	I don't mind what you use this code for! it wasn't made for professional use.
-	Instead, it was made for educational purposes and for fun.
-	Please hack this, change it and use it in any way you see fit.
-	Please, be acknowledged that I take no responsibility for anything bad that 
-	happens, to you as a result of your actions. However this code is protected by GNU GPLv3,
-	this means you must attribute me if you use it. Have fun and...
-	Cheers :D
-	
-~
-
- */
 using System;
 using System.Windows.Forms;
 

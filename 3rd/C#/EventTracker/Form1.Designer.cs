95e7ae6145db554eeb3c6bcb9f97296c1db7c0aa
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-11-05 22:21:03 +0200

    Add files via upload

---
+++ 3rd/C#/EventTracker/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace EventTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.RadioButton1_Click);
+            this.radioButton1.MouseEnter += new System.EventHandler(this.RadioButton1_MouseEnter);
+            this.radioButton1.MouseLeave += new System.EventHandler(this.RadioButton1_MouseLeave);
+            this.radioButton1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RadioButton1_MouseUp);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseClick);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseDown);
+            this.button1.MouseEnter += new System.EventHandler(this.Button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.Button1_MouseLeave);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TextBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            this.textBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TextBox1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.Label1_TextChanged);
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            this.label1.MouseEnter += new System.EventHandler(this.Label1_MouseEnter);
+            this.label1.MouseLeave += new System.EventHandler(this.Label1_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            this.panel1.DoubleClick += new System.EventHandler(this.Panel1_DoubleClick);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
+            this.panel1.MouseEnter += new System.EventHandler(this.Panel1_MouseEnter);
+            this.panel1.MouseLeave += new System.EventHandler(this.Panel1_MouseLeave);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.DropDown += new System.EventHandler(this.ComboBox1_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.comboBox1.DropDownClosed += new System.EventHandler(this.ComboBox1_DropDownClosed);
+            this.comboBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ComboBox1_MouseClick);
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button2_MouseClick);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button3_MouseClick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 3rd/C#/EventTracker/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~Button's Events~~~~~~~~~~~~~~~~~~~~//
+
+        private void Button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("You just pressed the button... THE BUTTON!");
+            MakeSomeHistory("Mouse clicked the Button on : ");
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            BackColor = Color.Pink;
+            label1.Text = ("You are holding down the button... THE BUTTON!");
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            label1.Text = "I guess a Label";
+        }
+
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkCyan;
+            MakeSomeHistory("Mouse entered the Button on : ");
+        }
+
+        private void Button1_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = default(Color);
+            MakeSomeHistory("Mouse left the Button on : ");
+        }
+
+
+
+
+
+
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~Label's Events~~~~~~~~~~~~~~~~~~~~//
+        private void Label1_MouseEnter(object sender, EventArgs e)
+        {
+            MakeSomeHistory("Mouse entered the Label on : ");
+        }
+
+        private void Label1_TextChanged(object sender, EventArgs e)
+        {
+            MakeSomeHistory("Label's text changed on : ");
+        }
+
+        private void Label1_MouseLeave(object sender, EventArgs e)
+        {
+            MakeSomeHistory("Mouse left the Label on : ");
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "You are holding me down...";
+            MakeSomeHistory("Mouse clicked the Label on : ");
+        }
+
+
+
+
+
+
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~TextBox's Events~~~~~~~~~~~~~~~~~~~~//
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //One key pressed means a new log into the history
+            MakeSomeHistory("TextBox's text changed on : ");
+        }
+
+        private void TextBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Text = textBox1.Text;
+            MakeSomeHistory("Mouse clicked the TextBox on : ");
+        }
+
+        private void TextBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            label1.Text = "The cake is a lie! The cake is a lie! The cake is a lie!";
+            MakeSomeHistory("Mouse double-clicked the TextBox on : ");
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //When key is pressed, TextBox clicked and hit the enter button
+            MakeSomeHistory("User pressed the key : " +label1.Text + " on : ");
+        }
+
+
+
+
+
+
+
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~Panel's Events~~~~~~~~~~~~~~~~~~~~//
+
+
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.BackColor = Color.DarkBlue;
+            MakeSomeHistory("Mouse clicked the panel on : ");
+        }
+
+        private void Panel1_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkRed;
+            MakeSomeHistory("Mouse entered the panel on : ");
+        }
+
+        private void Panel1_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = default;
+            MakeSomeHistory("Mouse left the panel on : ");
+        }
+
+        private void Panel1_DoubleClick(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Purple;
+            MakeSomeHistory("Mouse  double-clicked the panel on : ");
+        }
+
+
+
+
+
+
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~RadioButton's Events~~~~~~~~~~~~~~~~~~~~//
+        private void RadioButton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Helf Life 3 Confirmed");
+            MakeSomeHistory("Mouse clicked on the RadioButton on : ");
+        }
+
+        private void RadioButton1_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "Portal 3 Confirmed";
+            MakeSomeHistory("Mouse unclicked on the RadioButton on : ");
+        }
+
+        private void RadioButton1_MouseEnter(object sender, EventArgs e)
+        {
+            MakeSomeHistory("Mouse entered  the RadioButton on : ");
+        }
+
+        private void RadioButton1_MouseLeave(object sender, EventArgs e)
+        {
+            MakeSomeHistory("Mouse left  the RadioButton on : ");
+        }
+
+
+
+
+
+
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~ComboBox's Events~~~~~~~~~~~~~~~~~~~~//
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MakeSomeHistory("User choosed : " + comboBox1.Text + " on: ");
+        }
+
+        private void ComboBox1_DropDown(object sender, EventArgs e)
+        {
+            MakeSomeHistory("User opened the ComboBox (Drop Down) on : ");
+        }
+
+        private void ComboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            MakeSomeHistory("User closed the ComboBox (Drop Down) on : ");
+        }
+
+        private void ComboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MakeSomeHistory("Mouse clicked on the ComboBox (Drop Down) on : ");
+        }
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        /*
+         Event's History
+        */
+        //path is a global var that contains the path for the txt file named history.txt
+        string path = Environment.CurrentDirectory + "/" + "history.txt";
+        private void Button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            string path = Environment.CurrentDirectory + "/" + "history.txt";
+            if(!File.Exists(path))
+            {
+                File.CreateText(path);
+                richTextBox1.Text = "Text File history.txt created";
+            }
+            else
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    string text = "";
+                    string line;
+                    //Read lines from history.txt up until you find null
+                    while (true)
+                    {
+                        line = sr.ReadLine();
+                        //Old lines = Old Lines + new line + new empty line
+                        text = text + line + "\n";
+                        //Break the loop if you find null line
+                        if(line == null)
+                        {
+                            break;
+                        }
+                    }
+                    //Print all the history to RichTextBox
+                    richTextBox1.Text = text;
+
+                }
+            }
+        }
+
+        private void  MakeSomeHistory(string x)
+        {
+            //Create new line on the txt file and 
+            File.AppendAllText(path, x + DateTime.Now.ToString() + "\n");
+        }
+
+        private void Button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(File.Exists(path))
+            {
+                //Delete the history.txt and print the message
+                //File.Delete(path);
+
+                //Emptying rthe txt file instead of deleting it
+                //There is a bug if delete txt show history and again choose clear
+                File.WriteAllText(path, String.Empty);
+                richTextBox1.Text = "History Deleted";
+            }
+        }
+
+    }
+}

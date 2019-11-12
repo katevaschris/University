4db8c0a1ececc280b54bad1854a9604a95aa0b36
Chris Katevas <44655162+katevaschris@users.noreply.github.com>
2019-11-12 22:12:17 +0200

    Add files via upload

---
+++ 3rd/C#/RandomLines/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RandomLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 602);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Random Lines";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 3rd/C#/RandomLines/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomLines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Pen p;
+        Graphics g;
+        Random r;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Blue);
+            g = this.CreateGraphics();
+            r = new Random();
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //Set a value to random angle
+            an = r.Next(0, 360);
+            //Generate the random angle
+            cos = (float)Math.Cos(an);
+            sin = (float)Math.Sin(an);
+            g.DrawLine(p, x, y, x + cos* 100, y + sin * 100);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+            //Mouse cordinates (global var)
+            x = e.X;
+            y = e.Y;
+        }
+        int x, y;
+        float cos, sin, an;
+
+    }
+    
+}
